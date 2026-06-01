3696495d31bf5ce8c5d72538a1eda16a84eddac8
Oscar <oscar1056gm@gmail.com>
2026-06-01 16:17:52 +0200

    fix: Spline component expliciete positie en afmetingen

--- components/marketing/hero-spline.tsx
+++ components/marketing/hero-spline.tsx
@@ -11,11 +11,16 @@ export function HeroSpline() {
         zIndex: 0,
         pointerEvents: 'none',
         overflow: 'hidden',
+        width: '100%',
+        height: '100%',
       }}
     >
       <Spline
         scene="https://prod.spline.design/CF4X-Ifi1K34A4O8/scene.splinecode"
         style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
           width: '100%',
           height: '100%',
           pointerEvents: 'none',
